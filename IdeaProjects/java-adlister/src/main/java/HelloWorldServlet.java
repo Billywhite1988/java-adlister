798c3596d1bd87e41fc56c3bc674c0f4b2cc396f
Billy <billy_white.88@yahoo.com>
2018-03-27 23:17:25 -0500

    Completed Servlets Exercise

---
+++ IdeaProjects/java-adlister/src/main/java/HelloWorldServlet.java
@@ -0,0 +1,24 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    private int count = 0;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        String name = req.getParameter("name");
+        if (name != null) {
+            out.println("<h1>Hello, " + name + "</h1>");
+        } else {
+            out.println("<h1 style=\"font-family: fantasy;\">Hello World</h1>");
+        }
+        count += 1;
+        out.println("The count is " + count);
+    }
+}
